51df534ab4199ed9de1450ac308033bbe1926d7e
DiegoA-R-G <diegopulgonilza28@gmail.com>
2024-01-22 08:53:37 -0500

    editar soap

--- src/test/java/co.com.sofka/runners/GetSoapRunnerTest.java
+++ src/test/java/co.com.sofka/runners/GetSoapRunnerTest.java
@@ -5,13 +5,10 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-
-@CucumberOptions(
-        features ="src/test/resources/features/",
+@CucumberOptions( features ="src/test/resources/features/",
         glue = "co.com.sofka.stepdefinitions",
         snippets = CucumberOptions.SnippetType.CAMELCASE,
         plugin = "pretty",
-        tags = "@VerFavoritos"
-)
-public class FormRunnerTest {
+        tags = "@SumarSoap")
+public class GetSoapRunnerTest {
 }
