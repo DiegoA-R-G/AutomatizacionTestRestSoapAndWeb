fb708df7c2fd99610ee1e1fd5061cd5c26c9ff04
DiegoA-R-G <diegopulgonilza28@gmail.com>
2024-01-22 09:03:45 -0500

    Funcional

--- src/test/java/co.com.sofka/runners/FormRunnerTest.java
+++ src/test/java/co.com.sofka/runners/FormRunnerTest.java
@@ -10,8 +10,7 @@ import org.junit.runner.RunWith;
         features ="src/test/resources/features/",
         glue = "co.com.sofka.stepdefinitions",
         snippets = CucumberOptions.SnippetType.CAMELCASE,
-        plugin = "pretty",
-        tags = "@VerFavoritos"
+        plugin = "pretty"
 )
 public class FormRunnerTest {
 }
